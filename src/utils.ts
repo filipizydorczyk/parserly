281e398ae8b8e3314422b9eb4c1b631056801d1f
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-11-12 17:21:35 +0100

    dev: or concept

--- src/utils.ts
+++ src/utils.ts
@@ -5,70 +5,80 @@
  * `ParserCombinator.from().bold().fullWords().charsAround().build()` 
  */
 export class ParserCombinator {
-    private regex: string;
+  protected regex: string;
 
-    /**
-     * @returns `ParserCombinator` instance
-     */
-    static from() {
-        return new ParserCombinator("");
-    }
+  /**
+   * @returns `ParserCombinator` instance
+   */
+  static from() {
+    return new ParserCombinator("");
+  }
 
-    /**
-     * Private constructor
-     * @param regex string to begin with
-     */
-    private constructor(regex: string) {
-        this.regex = regex;
-    }
+  static or(options: ParserCombinator[]) {
+    return new ParserCombinator(
+      `(?:${options.map((option) => `(?:${option.getRegexString()})`).join("|")})`
+    );
+  }
 
-    /**
-     * Creates string that mathces markdown bolds
-     * and append this.
-     * @returns current `ParserCombinator` insatnce
-     */
-    public bold() {
-        this.regex = `${this.regex}(?:\\*\\*[^\\s].*?[^\\s]\\*\\*)`;
-        return this;
-    }
+  /**
+   * Private constructor
+   * @param regex string to begin with
+   */
+  private constructor(regex: string) {
+    this.regex = regex;
+  }
 
-    public italic() {
-        this.regex = `${this.regex}(?:[^\\*]\\*[^\\s].*?[^\\s]\\*[^\\*])`;
-        return this;
-    }
+  /**
+   * Creates string that mathces markdown bolds
+   * and append this.
+   * @returns current `ParserCombinator` insatnce
+   */
+  public bold() {
+    this.regex = `${this.regex}(?:\\*\\*[^\\s].*?[^\\s]\\*\\*)`;
+    return this;
+  }
 
-    /**
-     * Takes current regex and adds positive lookahed
-     * and lookbehind part.
-     * @returns current `ParserCombinator` insatnce
-     */
-    public charsAround() {
-        this.regex = `(?<=)(${this.regex})(?=)`;
-        return this;
-    }
+  public italic() {
+    this.regex = `${this.regex}(?:[^\\*]\\*[^\\s].*?[^\\s]\\*[^\\*])`;
+    return this;
+  }
 
-    /**
-     * Takes current regex and will make sure it will match
-     * full word or words. For example if we create regex
-     * `ParserCombinator.from().bold().build()` it will match
-     * `**dsasd dasda**` but also `dasdas**sads**dasdsa`.
-     * If we use this method on top of that
-     * (`ParserCombinator.from().bold().fullWords().build()`) it
-     * will not parse `dasdas**sads**dasdsa` anymore.
-     * It will work when there are spacec around, begind on the
-     * beginning of line, finishes with end of line, comma or dot.
-     * @returns current `ParserCombinator` insatnce
-     */
-    public fullWords() {
-        this.regex = `(?:^|\\s)(?:${this.regex})(?:$|\\s|\\.|\\,)`;
-        return this;
-    }
+  /**
+   * Takes current regex and adds positive lookahed
+   * and lookbehind part.
+   * @returns current `ParserCombinator` insatnce
+   */
+  public charsAround() {
+    this.regex = `(?<=)(${this.regex})(?=)`;
+    return this;
+  }
 
-    /**
-     * Takes current string and creates regex out of that
-     * @returns `RegExp`
-     */
-    public build() {
-        return new RegExp(this.regex);
-    }
+  /**
+   * Takes current regex and will make sure it will match
+   * full word or words. For example if we create regex
+   * `ParserCombinator.from().bold().build()` it will match
+   * `**dsasd dasda**` but also `dasdas**sads**dasdsa`.
+   * If we use this method on top of that
+   * (`ParserCombinator.from().bold().fullWords().build()`) it
+   * will not parse `dasdas**sads**dasdsa` anymore.
+   * It will work when there are spacec around, begind on the
+   * beginning of line, finishes with end of line, comma or dot.
+   * @returns current `ParserCombinator` insatnce
+   */
+  public fullWords() {
+    this.regex = `(?:^|\\s)(?:${this.regex})(?:$|\\s|\\.|\\,)`;
+    return this;
+  }
+
+  public getRegexString() {
+    return this.regex;
+  }
+
+  /**
+   * Takes current string and creates regex out of that
+   * @returns `RegExp`
+   */
+  public build() {
+    return new RegExp(this.regex);
+  }
 }
